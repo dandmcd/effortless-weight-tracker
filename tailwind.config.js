6e07e85319df0cea43de1922bad06899ddb08134
givionte <dandmcd@live.com>
2021-09-28 23:40:59 -0500

    PurgeCSS now correctly purges Tailwinds utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.js'],
+  purge: {
+    enabled: true,
+    content: ['./public/**/*.html', './src/**/*.js', './src/**/*.ts'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
